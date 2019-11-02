be3eae7a972099db80a5ab34e2cf8f973a37c979
Ben Leech <b.leech@hotmail.ca>
2019-11-02 11:31:25 -0600

    form to submit needle pickup

--- angular/src/app/admin/needle-pickup/needle-pickup.component.ts
+++ angular/src/app/admin/needle-pickup/needle-pickup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Pickup} from "../../data/pickup";
 
 @Component({
   selector: 'app-needle-pickup',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NeedlePickupComponent implements OnInit {
 
-  constructor() { }
+  public formGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.formGroup = this.initFormGroup(null);
+  }
+
+  public submitPickup() {
+    console.log("submitted pickup: ", this.formGroup.getRawValue() as Pickup);
+  }
+
+  private initFormGroup(requestId: number): FormGroup {
+    return this.fb.group({
+      id: requestId,
+      count: 0
+    });
   }
 
 }
--- angular/src/app/data/pickup-request.ts
+++ angular/src/app/data/pickup-request.ts
@@ -1,4 +1,5 @@
 export class PickupRequest {
+  id?: number;
   description: string;
   count: number;
   phoneNumber: number;
