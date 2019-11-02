71b8d45a477bfa42479e1e429facc82c16e58cb6
Ben Leech <b.leech@hotmail.ca>
2019-11-01 23:03:32 -0600

    adding layer for geoposition

--- angular/src/app/map/map.component.ts
+++ angular/src/app/map/map.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {Map, View} from 'ol';
+import {Component, OnInit} from '@angular/core';
+import {Feature, Geolocation, Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import {transform, transformExtent} from 'ol/proj';
+import VectorSource from 'ol/source/vector';
+import Vector from 'ol/layer/vector';
+import {Point} from 'ol/geom';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
 
 @Component({
   selector: 'app-map',
@@ -13,6 +17,10 @@ export class MapComponent implements OnInit {
 
   public map: Map;
 
+  private view: View;
+  private geolocation: Geolocation;
+  private currentPositionLayer: Vector;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,6 +28,12 @@ export class MapComponent implements OnInit {
   }
 
   private initMap(): void {
+    this.view = new View({
+      center: transform([-104.6189, 50.4452], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 11.5,
+      extent: transformExtent([-104.4189, 50.3452, -104.8189, 50.5452], 'EPSG:4326', 'EPSG:3857')
+    });
+
     this.map = new Map({
       target: 'map',
       layers: [
@@ -27,10 +41,45 @@ export class MapComponent implements OnInit {
           source: new OSM()
         })
       ],
-      view: new View({
-        center: transform([-104.6189, 50.4452], 'EPSG:4326', 'EPSG:3857'),
-        zoom: 11.5,
-        extent: transformExtent([-104.4189, 50.3452, -104.8189, 50.5452], 'EPSG:4326', 'EPSG:3857')
+      view: this.view
+    });
+
+    this.initGeolocation();
+  }
+
+  private initGeolocation() {
+    this.geolocation = new Geolocation({
+      trackingOptions: {
+        enableHighAccuracy: true
+      },
+      projection: 'EPSG:4326'
+    });
+
+    this.geolocation.setTracking(true);
+    this.geolocation.on('change:position', this.addCurrentLocationMarker());
+  }
+
+  private addCurrentLocationMarker(): void {
+    console.log(this.geolocation);
+    if(!this.geolocation || !this.geolocation.getPosition()) {
+      return;
+    }
+    
+    const positionMarker = new Feature({
+      geometry: new Point(this.geolocation.getPosition()),
+      style: new Style({
+        image: new CircleStyle({
+          radius: 6,
+          fill: new Fill({color: '#3399CC'}),
+          stroke: new Stroke({ color: '#fff', width: 2 })
+        })
+      })
+    });
+
+    this.currentPositionLayer = new Vector({
+      map: this.map,
+      source: new VectorSource({
+        features: [positionMarker]
       })
     });
   }
