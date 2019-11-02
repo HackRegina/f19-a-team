e6ad52726dc0528145d69be16e6ff03c960fae93
Ben Leech <b.leech@hotmail.ca>
2019-11-02 10:33:19 -0600

    getting navbar working

--- angular/src/app/common/header/header.component.ts
+++ angular/src/app/common/header/header.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public menuOpen: boolean;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleMenu() {
+    this.menuOpen = !this.menuOpen;
   }
 
 }
