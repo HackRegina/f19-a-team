91828944f759bdb0c952d4b69a7f503e8e01c5de
Assisting <erik@eriklabine.com>
2019-11-01 21:48:57 -0600

    Initial commit of the backend stuff

---
+++ backend/NeedleBuddy.DB/ViewModels/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeedleBuddy.DB.ViewModels
+{
+    public class Enums
+    {
+        public enum PickupStatus
+        {
+            Pending,
+            NotFound,
+            Found
+        }
+    }
+}
---
+++ backend/NeedleBuddy.DB/ViewModels/PickupRequestViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeedleBuddy.DB.ViewModels
+{
+    public class PickupRequestViewModel
+    {
+        public int ID { get; set; }
+        public string Phone_Number { get; set; }
+        public string Description { get; set; }
+        public int Count { get; set; }
+        public Enums.PickupStatus Status { get; set; }
+        public float Location_Lat { get; set; }
+        public float Location_Long { get; set; }
+
+//        ID: Integer
+//Phone_Number: String
+//Description: String(could be almost anything)
+//Count: Integer(usually 0 except when in reporting-mode)
+//Status: Enum.PickupStatus
+//Location_Lat: Float
+//Location_Long: Float
+
+
+            
+    }
+}
