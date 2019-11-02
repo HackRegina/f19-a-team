e0de6e1f38da558ba96e0231fbb9ca3a80e6ab11
Ben Leech <b.leech@hotmail.ca>
2019-11-02 08:58:52 -0600

    added admin routes

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {MapComponent} from "./common/map/map.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {PickupComponent} from "./user/pickup/pickup.component";
 import {PickupRequestComponent} from "./user/pickup-request/pickup-request.component";
+import {AdminMapComponent} from "./admin/admin-map/admin-map.component";
 
 const routes: Routes = [
   { path: 'map', component: PickupComponent },
   { path: 'request', component: PickupRequestComponent },
+  { path: 'admin' , children: [
+      { path: 'pickup', component: AdminMapComponent },
+      { path: '', redirectTo: '/admin/pickup', pathMatch: 'full' }
+    ]},
   { path: '', redirectTo: '/map', pathMatch: 'full' }
 ];
 
