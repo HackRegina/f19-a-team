ad0d74f0a2430fdad00a2879e57e6307d6d7dcb9
Ben Leech <b.leech@hotmail.ca>
2019-11-01 21:36:47 -0600

    added tailwindcss

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MapComponent} from "./map/map.component";
 import {PickupComponent} from "./pickup/pickup.component";
+import {PickupRequestComponent} from "./pickup-request/pickup-request.component";
 
 const routes: Routes = [
   { path: 'map', component: PickupComponent },
+  { path: 'request', component: PickupRequestComponent },
   { path: '', redirectTo: '/map', pathMatch: 'full' }
 ];
 
