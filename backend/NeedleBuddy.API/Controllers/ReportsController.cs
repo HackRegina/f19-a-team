8c9470eb4f2152815be9969843f635104b8b95e0
Majid <Alikhanimjd@gmail.com>
2019-11-02 11:16:31 -0600

    Reports Controller updated

--- backend/NeedleBuddy.API/Controllers/ReportsController.cs
+++ backend/NeedleBuddy.API/Controllers/ReportsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NeedleBuddy.DB;
@@ -14,15 +15,19 @@ namespace NeedleBuddy.API.Controllers
     public class ReportsController : ControllerBase
     {
         private IRepository _repository;
-        public ReportsController(IRepository repository)
+        private IMapper _mapper;
+        public ReportsController(IRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         [HttpGet()]
         public List<PickupRequestViewModel> ReportWithinDateRange(DateTime StartDate, DateTime EndDate)
         {
-            throw new NotImplementedException();
+            List<PickupRequestViewModel> reportsView = new List<PickupRequestViewModel>();
+            List<Pickuprequest> reports = _repository.FindPickupRequestsInDateRange(StartDate, EndDate);
+            return _mapper.Map<List<PickupRequestViewModel>>(reports);
         }
     }
 }
