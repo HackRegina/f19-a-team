3940f961de83f87e54d14518bdbd63c5c3a77e43
Ben Leech <b.leech@hotmail.ca>
2019-11-01 19:55:03 -0600

    added header component and pickup page component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { HeaderComponent } from './header/header.component';
+import { PickupComponent } from './pickup/pickup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    HeaderComponent,
+    PickupComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/src/app/pickup/pickup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pickup',
+  templateUrl: './pickup.component.html',
+  styleUrls: ['./pickup.component.scss']
+})
+export class PickupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
