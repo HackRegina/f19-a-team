d5d985dd1a9285042a5981148f357f19a9aa9b34
Ben Leech <b.leech@hotmail.ca>
2019-11-02 11:51:03 -0600

    added login form

--- angular/src/app/admin/login/login.component.ts
+++ angular/src/app/admin/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public formGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.formGroup = this.initFormGroup();
+  }
+
+  public login(): void {
+
+  }
+
+  private initFormGroup(): FormGroup {
+    return this.fb.group({
+      username: null,
+      password: null
+    })
   }
 
 }
--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {RequestMapComponent} from "./user/request-map/request-map.component";
 import {PickupRequestComponent} from "./user/pickup-request/pickup-request.component";
 import {AdminMapComponent} from "./admin/admin-map/admin-map.component";
 import {NeedlePickupComponent} from "./admin/needle-pickup/needle-pickup.component";
+import {LoginComponent} from "./admin/login/login.component";
 
 const routes: Routes = [
   { path: 'map', component: RequestMapComponent },
   { path: 'request', component: PickupRequestComponent },
   { path: 'admin' , children: [
+      { path: 'login', component: LoginComponent },
       { path: 'pickup-map', component: AdminMapComponent },
       { path: 'needle-pickup', component: NeedlePickupComponent },
       { path: '', redirectTo: '/admin/pickup-map', pathMatch: 'full' }
