2666ae2b9465578abfc7ca7158c4437ea8e6a268
Assisting <erik@eriklabine.com>
2019-11-01 23:43:59 -0600

    Context and DB model objects

---
+++ backend/NeedleBuddy.DB/NeedleBuddyContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace NeedleBuddy.DB
+{
+    public partial class NeedleBuddyContext : DbContext
+    {
+        public NeedleBuddyContext()
+        {
+        }
+
+        public NeedleBuddyContext(DbContextOptions<NeedleBuddyContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Dropofflocation> Newtable { get; set; }
+        public virtual DbSet<Pickuprequest> Pickuprequest { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Dropofflocation>(entity =>
+            {
+                entity.HasNoKey();
+            });
+
+            modelBuilder.Entity<Pickuprequest>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ backend/NeedleBuddy.DB/Pickuprequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NeedleBuddy.DB
+{
+    [Table("pickuprequest")]
+    public partial class Pickuprequest
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("phone_number")]
+        [StringLength(24)]
+        public string PhoneNumber { get; set; }
+        [Column("description")]
+        [StringLength(1000)]
+        public string Description { get; set; }
+        [Column("count")]
+        public int? Count { get; set; }
+        [Column("status")]
+        [StringLength(8)]
+        public string Status { get; set; }
+        [Column("location_lat")]
+        public double? LocationLat { get; set; }
+        [Column("location_long")]
+        public double? LocationLong { get; set; }
+    }
+}
