6b8bcb14a736fb7675ba02934475aff9e5425c22
Ben Leech <b.leech@hotmail.ca>
2019-11-02 14:54:11 -0600

    adding auth to frontend

--- angular/src/app/services/auth.service.ts
+++ angular/src/app/services/auth.service.ts
@@ -1,13 +1,38 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../data/user";
+import {BehaviorSubject, Observable} from "rxjs";
+import {map} from "rxjs/operators";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
-  public login(): void {
-    console.log("logging in");
+  constructor(private httpClient: HttpClient) { }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.httpClient.post<any>(`${environment.baseUrl}/users/auth`, { username, password })
+      .pipe(map(user => {
+        if (user && user.token) {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+
+        return user;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
   }
 }
